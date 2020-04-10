1d55dc61eb6fb45c4df75322c9fe6b88574b1c0f
Oleh Hrechukh <grechukh619@gmail.com>
2020-04-10 21:05:23 +0300

    Added workflow

---
+++ CangguEvents.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CangguEvents.Domain.Properties
+{
+    [CompilerGenerated]
+    internal static class GitVersionInformation
+    {
+        public static string Major = "1";
+        public static string Minor = "1";
+        public static string Patch = "1";
+    }
+}
